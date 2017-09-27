4b46a72e2650de508e3bb76aabf0e66e402c336b
Ben Blue Groneman <bgroneman@localhost.localdomain>
2017-09-27 16:05:21 -0600

    initial checkin

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-var path = require('path');
 var express = require('express');
+var models = require('./models');
+var bodyparser = require('bodyparser');
+var Sequelize = require('sequelize');
+var config = require('./config/config.json');
 var app = express();
 var router = express.Router();
+
+
+var app = express();
+app.use(bodyparser.json());
+
+
+// pass in Force: true to clear tables
+models.User.sync({}).then(function () {
+    // Table created
+    return models.User.create({
+    });
+});
+
 
 // Potentially necessary? Harmless to leave until the end of our testing...
 app.use(function(req, res, next) {
@@ -13,33 +29,33 @@ router.get('/', function(req, res) {
     res.json({message: 'hoorway! welcome to our api!'});
 });
 
-// SQL section for connecting to database
-const sqlite3 = require('sqlite3').verbose();
-let db = new sqlite3.Database('./db/skiscores.db', (err) => {
-    if (err) {
-        console.error(err.message);
-    }
-    console.log('Connected to the skiscores database.');
-});
-db.serialize(() => { // Example query
-    db.each('Select id, location, date from races', (err, row) => { 
-        if (err) {
-            console.error(err.message);
-        }
-        console.log(row.id + '\t' + row.location + '\t' + row.name);
-    });
-});
-db.close((err) => {
-    if (err) {
-        return console.error(err.message);
-    }
-    console.log('Closed the database connection.');
-});
-
-// Finalize API settings and initialize
-app.use('/api', router);
-app.set('port', 8000);
-var server = app.listen(app.get('port'), function() {
-        var port = server.address().port;
-        console.log('Magic happens on port ' + port);
-});
+// var sqlite3 = require('sqlite3').verbose();
+// let db = new sqlite3.Database('./db/skiscores.db', (err) => {
+//     if (err) {
+//         console.error(err.message);
+//     }
+//     console.log('Connected to the skiscores database.');
+// });
+// db.serialize(() => { // Example query
+//     db.each('Select id, location, date from races', (err, row) => {
+//         if (err) {
+//             console.error(err.message);
+//         }
+//         console.log(row.id + '\t' + row.location + '\t' + row.name);
+//     });
+// });
+// db.close((err) => {
+//     if (err) {
+//         return console.error(err.message);
+//     }
+//     console.log('Closed the database connection.');
+// });
+//
+//
+// // Finalize API settings and initialize
+// app.use('/api', router);
+// app.set('port', 8000);
+// var server = app.listen(app.get('port'), function() {
+//         var port = server.address().port;
+//         console.log('Magic happens on port ' + config.port);
+// });
---
+++ models/racemodel.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var Sequelize = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+    var User = sequelize.define('user', {
+        Name: {
+            type: Sequelize.STRING,
+            field: 'first_name' // Will result in an attribute that is firstName when user facing but first_name in the database
+        },
+        lastName: {
+            type: Sequelize.STRING
+        }
+    }, {
+        freezeTableName: true // Model tableName will be the same as the model name
+    });
+
+    User.sync({force: true}).then(function () {
+        // Table created
+        return User.create({
+            firstName: 'John',
+            lastName: 'Hancock'
+        });
+    });
+};
