6f1690ca1efb8587a57b500493f1d4ba7908a79f
Ryan Buchmeier <rwbleo@gmail.com>
2017-10-09 08:53:13 -0600

    Create Person Model

---
+++ models/Person.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var Sequelize = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+    const person = sequelize.define('person', {
+        personid: {
+            type: Sequelize.INTEGER,
+            unique: true,
+            primaryKey: true
+        },
+        gender: {
+            type: Sequelize.STRING(1) // M, F, U
+        },
+        first: {
+            type: Sequelize.STRING(15) // M, F, U
+        },
+        last: {
+            type: Sequelize.STRING(15)
+        },
+        graduation: {
+            type: Sequelize.INTEGER
+        }
+    }, {
+        freezeTableName: true // Model tableName will be the same as the model name
+    });
+
+    return person
+};
